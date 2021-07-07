75cdba6ff80fb6b2b517d471c443817ddac96fd4
Yegor Savoshevich <yegorsavoshevich@protonmail.com>
2021-07-07 14:03:21 +0200

    test

--- is-auth.js
+++ is-auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const authHeader = req.get("Authorization");
+    console.log("test");
     if (!authHeader) {
       const error = new Error("Not authenticated.");
       error.statusCode = 401;
